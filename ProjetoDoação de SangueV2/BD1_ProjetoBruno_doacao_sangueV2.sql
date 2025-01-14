506564120ce3bd9f314a1928bbf703d00441f5ec
GabrielDonizete13 <138245920+Gabriel-Donizete3@users.noreply.github.com>
2025-01-13 21:51:22 -0300

    Segunda Versão do Programa

---
+++ ProjetoDoação de SangueV2/BD1_ProjetoBruno_doacao_sangueV2.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE doacao_sangue;
+
+CREATE TABLE doador_financeiro(
+idDoadorFinanceiro INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+cpf VARCHAR (12) NOT NULL,
+data_nascimento VARCHAR (10),
+logradouro VARCHAR (50),
+bairro VARCHAR (50),
+municipio VARCHAR(50),
+unidade_federativa VARCHAR (50),
+telefone VARCHAR (11),
+tipo_doador VARCHAR (10)
+);
+
+CREATE TABLE doador_de_sangue(
+idDoadorSangue INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+cpf VARCHAR (12) NOT NULL,
+data_nascimento VARCHAR (10),
+logradouro VARCHAR (50),
+bairro VARCHAR (50),
+municipio VARCHAR(50),
+unidade_federativa VARCHAR (50),
+telefone VARCHAR (11),
+tipo_sanguineo VARCHAR (3),
+peso_kg FLOAT (3)
+);
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+
+SELECT * FROM doador_financeiro;
+DROP TABLE doador_financeiro;
+INSERT INTO doador_financeiro (
+cpf,data_nascimento,logradouro,bairro,municipio,
+unidade_federativa,telefone,tipo_doador) VALUES
+("999999999-99","2005-05-13","Praça do Zezinho","Algum Bairro
+","Algum Município","MG","99999999999","Mensal");
+
+
+SELECT * FROM doador_de_sangue;
+DROP TABLE doador_de_sangue;
+INSERT INTO doador_de_sangue (
+cpf,data_nascimento,logradouro,bairro,municipio,
+unidade_federativa,telefone,tipo_sanguineo,peso_kg) VALUES
+("999999999-99","2005-05-13","Praça do Zezinho","Algum Bairro
+","Algum Município","MG","99999999999","AB+",80.7);
+
---
+++ ProjetoDoação de SangueV2/README.txt
@@ -0,0 +1,9 @@
+Essa é a segunda versão do código do projeto, foram implementadas a ligação com o banco de dados. Favor, criar um banco de dados com uma database correspondente, e fazer a devida conexão para que o código funcione.
+
+Problemas encontrados:
+
+- Banco de dados incompleto, necessita de Querys para consulta.
+- Interface gráfica incompleta, falta a interface de consulta dos comprovantes.
+- Não há funções nos botões nem nos campos de texto da interface gráfica.
+- Por hora, a entrada de dados nas tabelas não enviam informações para o banco de dados.
+
